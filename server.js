22955e26a9e1d83722be19ecee7c9e8ab0f2bb0a
zeyad-kay <zeyad_khaled@outlook.com>
2020-12-12 21:34:13 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});
